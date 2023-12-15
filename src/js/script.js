9a271dd95e7b9b399f0019f2a55a38be6699613d
litu3000 <acersytec@i.ua>
2023-12-15 21:23:02 +0200

    HW 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// Task #1
+let admin;
+let name= 'John';
+admin = name;
+alert(admin);
+
+
+
+
+
+// Task #2
+let world ;
+let user ;
+
+
+
+
+// Task #3
+let task;
+task = '10' + 10;
+console.log(task);
+console.log(typeof 10);
+console.log(typeof '10');
+console.log(typeof true);
+console.log(typeof undeclaredVariable);
