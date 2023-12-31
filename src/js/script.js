63043829b00a1bf7211263c15ccc76ec6a749e4a
litu3000 <acersytec@i.ua>
2023-12-31 15:17:51 +0200

    Initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+// Task#8
+const num1 = Number(prompt('число 1'));
+const num2 = Number(prompt('число 2'));
+const num3 = Number(prompt('число 3'));
+
+function average (a,b,c){
+    return (a + b + c)/3
+}
+alert(average(num1,num2,num3));
