b6b3d3ea072c5a83d97d5cf6fdee389d944d10ed
litu3000 <acersytec@i.ua>
2023-12-31 15:23:15 +0200

    Correct

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+// Task#9
+
+const mode1 = Number(prompt('Введите часы'));
+const mode2 = Number(prompt('Введите минуты'));
+function time(a, b) {
+    let output;
+    output = (a * 3600) + (b * 60);
+    return output;
+}
+
+const data = time(mode1, mode2)
+alert(data)
