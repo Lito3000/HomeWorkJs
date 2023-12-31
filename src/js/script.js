d60dd51d97ba324e7508668cbbcd996ae539d1ad
litu3000 <acersytec@i.ua>
2023-12-31 15:26:16 +0200

    Correct

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const action = (prompt('Что ты хочешь сделать? + - / *'));
+const val1 = Number(prompt('Введите число'))
+const val2 = Number(prompt('Введите число'))
+
+function calc(action, val1, val2) {
+    if (action === '+') {
+        return val1 + val2
+    } else if (action === '-') {
+        return val1 - val2
+    } else if (action === '/') {
+        return val1 / val2
+    } else if (action === '*') {
+        return val1 * val2
+    }
+}
+
+const multi = calc(action, val1, val2)
+console.log(multi)
