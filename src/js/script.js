8a69b83cc208a25523750a335677b7a1a91aeece
litu3000 <acersytec@i.ua>
2024-01-02 12:42:52 +0200

    Correct

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,7 @@
 const mode1 = Number(prompt('Введите часы'));
 const mode2 = Number(prompt('Введите минуты'));
 function time(a, b) {
-    let output;
-    output = (a * 3600) + (b * 60);
-    return output;
+    return (a * 3600) + (b * 60);
 }
 
 const data = time(mode1, mode2)
