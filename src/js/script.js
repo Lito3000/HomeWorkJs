3a8f2635bfa83031f3de485d099dab5052c14e9e
litu3000 <acersytec@i.ua>
2024-01-06 21:39:17 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let a = 3;
+let b = 3;
+
+a === 0 ? console.log('Правильно'): console.log('Неверно');
+a > 0 ? console.log('Правильно'): console.log('Неверно');
+a < 0 ? console.log('Правильно'): console.log('Неверно');
+a >= 0 ? console.log('Правильно'): console.log('Неверно');
+a <= 0 ? console.log('Правильно'): console.log('Неверно');
+a !== 0 ? console.log('Правильно'): console.log('Неверно');
+a === 'test' ? console.log('Правильно'): console.log('Неверно');
+a === 1 ? console.log('Правильно'): console.log('Неверно');
+a > 0 && a < 5 ? console.log('Правильно'): console.log('Неверно');
+a === 0 || a === 2 ? console.log(a + 7): console.log(a / 10);
+a === 1 || a < 1 && b >= 3 ? console.log(a + b): console.log(a - b);
+a > 2 && a < 11 || b >= 6 && b < 14 ? console.log('Верно'): console.log('Неверно');
+
+let c = 1;
+let d = 2;
+let e = 3;
+let f = 4;
+
+
+
+let num =
+c === 1 ? console.log('Зима'): console.log('Неверно');
