62c01ac8288c8ecd379e90ae5d1c37eba21e52ef
litu3000 <acersytec@i.ua>
2024-01-06 21:43:57 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -16,12 +16,21 @@ a === 0 || a === 2 ? console.log(a + 7): console.log(a / 10);
 a === 1 || a < 1 && b >= 3 ? console.log(a + b): console.log(a - b);
 a > 2 && a < 11 || b >= 6 && b < 14 ? console.log('Верно'): console.log('Неверно');
 
-let c = 1;
-let d = 2;
-let e = 3;
-let f = 4;
 
 
+let num = Number(prompt('input number or string'));
 
-let num =
-c === 1 ? console.log('Зима'): console.log('Неверно');
+
+switch (num){
+    case 1:
+        console.log('Зима');
+        break;
+    case 2:
+        console.log('Весна');
+        break;
+    case 3:
+        console.log('Лiто');
+        break;
+    case 4:
+        console.log('Осiнь')
+}
