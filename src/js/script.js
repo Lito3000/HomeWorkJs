a8a2085d340d7b8965c357fa620ad8a3d37c6f3d
litu3000 <acersytec@i.ua>
2024-01-07 11:09:42 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+// Task#1
+// let x = 1;
+// let y = 7;
+//
+// let result = x > y ? console.log('x більше за y'):console.log('x не більше, ніж y')
+
+// Task2#
+// let num = Number(prompt('Введите число'))
+//
+// if (num){
+//       Number(prompt('Введите число2'))
+// }
+
+
+// Task#3
+
+// let numberSum = prompt('Введите целое число');
+//
+// alert(numberSum.length)
+//
+// if (numberSum < 0 ){
+//     alert('Число' + ' ' + numberSum + ' ' + 'однозначне негативне')
+// }else {
+//     alert('Число' + ' ' + numberSum + ' ' + 'однозначне позитивне')
+// }
+
+// Task#4
+
+let sum1 = prompt('Введите целое число');
+let sum2 = prompt('Введите целое число');
+let sum3 = prompt('Введите целое число');
+
