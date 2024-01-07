651426fea863855eecb01c90ba0453ea0f331cb1
litu3000 <acersytec@i.ua>
2024-01-08 00:27:52 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,16 @@
 'use strict';
 
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+const numOrStr = prompt('input number or string');
 
-switch (true){
-    case numOrStr === null:
+
+switch (false){
+    case numOrStr !== null:
         console.log('ви скасували');
         break;
-    case numOrStr === '':
+    case numOrStr !== '':
         console.log('Empty String');
         break;
-    case isNaN( +numOrStr ):
+    case true !== isNaN( +numOrStr ):
         console.log(' number is Ba_NaN');
         break;
     default:
