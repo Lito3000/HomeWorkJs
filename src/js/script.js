11d072c559f41940f75202be62a51c6c664d8fc2
litu3000 <acersytec@i.ua>
2024-01-09 01:47:05 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+let line = 0;
+let sum = 0;
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+
+    } else if (i % 2 !== 0) {
+        line = line + i + ' ';
+        sum = sum + i;
+    }
+
+
+}
+console.log(line,sum)
+
