dcfa399bfc70b794dc3352f693f799a66ed1ebb4
litu3000 <acersytec@i.ua>
2024-01-08 23:26:09 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+
+const  userInput = prompt('Введте число')
+
+let num = null;
+if (typeof userInput === 'string'){
+    num = parseInt(userInput)
+    console.log(num)
+}else {
+    console.log('null',userInput)
+}
+
+
+let arr = [2,3,4]
+let outPut = null;
+
+if (num === 1){
+    outPut = `${num}` + ' ' + 'год'
+}else if (arr.includes(num)){
+    outPut = `${num}` + ' ' + 'годa'
+}else {
+    outPut = `${num}` + ' ' + 'лет'
+}
+
+alert(outPut)
