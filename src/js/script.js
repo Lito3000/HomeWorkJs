ad5a35a951294d69c6185b05dc9bfc744c92598a
litu3000 <acersytec@i.ua>
2024-01-08 12:45:46 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,20 @@
 // Task#16
 const year = Number(prompt('Твiй рік народження'));
 const city = prompt('в якому місті живеш');
-// const sport = prompt('Твiй улюблений вид спорту');
+const sport = prompt('Твiй улюблений вид спорту');
 
 const today = new Date().getFullYear();
 let age = 0;
 
 
 if (year) {
-    age = today - year;
+    age =  'Тобi' + ' ' + `${today - year}` + ' '+'роки';
 } else {
     age = 'Шкода, що Ви не захотіли ввести свію дату народження'
 }
-document.write(`${age}`);
 
-let userCity = null;//нул чтоб не было андефаинда
+
+let userCity = null;
 
 if (city === 'Kiev') {
     userCity = 'Ти живеш у столиці' + ' ' + city;
@@ -30,20 +30,24 @@ if (city === 'Kiev') {
     userCity = 'Шкода, що Ви не захотіли ввести свое мiсто';
 }
 
-// let userSport = null;
-//
-//
-// if (sport === 'Сноубординг') {
-//     alert('Круто! Хочеш стати' + ' ' + 'Алессандро Хеммерле' + '?')
-// } else if (sport === 'Фигурное катание') {
-//     alert('Круто! Хочеш стати' + ' ' + 'Луна Хендрикс' + '?')
-// } else if (sport === 'Гимнастика') {
-//     alert('Круто! Хочеш стати' + ' ' + 'Ребека Андраде' + '?')
-// } else {
-//     alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту')
-// }
+let userSport = null;
 
-//
+
+if (sport === 'Сноубординг') {
+    userSport = 'Круто! Хочеш стати' + ' ' + 'Алессандро Хеммерле' + '?';
+} else if (sport === 'Фигурное катание') {
+    userSport = 'Круто! Хочеш стати' + ' ' + 'Луна Хендрикс' + '?';
+} else if (sport === 'Гимнастика') {
+    userSport = 'Круто! Хочеш стати' + ' ' + 'Ребека Андраде' + '?';
+} else if (sport){
+    userSport = 'Твiй любимий вид спорту' + ' ' + sport;
+}else {
+    userSport = 'Шкода, що Ви не захотіли ввести свій улюблений вид спорту';
+}
+
+
 alert(
-    'Your age:' + `${age}` + 'year.' + '\n' + `${userCity}`
+   `${age}`  + '\n' + `${userCity}` + '\n' + `${userSport}`
 );
+
+document.write(`${age}`  + '</br>' + `${userCity}` + '</br>' + `${userSport}`);
