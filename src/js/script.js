199f438299ee7624669572f2a97509ca88d6fe6a
litu3000 <acersytec@i.ua>
2024-01-09 19:34:51 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const  elem = Number(prompt('Введите число'))
+const smallestDivisor = (num) => {
+
+    const iter = (num, acc) => {
+
+        if (num % acc === 0) return acc
+
+        return iter(num, acc + 1)
+    }
+
+    return iter(num, 2)
+}
+
+let h = smallestDivisor(elem)
+
+console.log(h)
