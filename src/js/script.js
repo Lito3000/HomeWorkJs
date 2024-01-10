75c6b345fb1ea5146d899ebf9a33960380adb553
litu3000 <acersytec@i.ua>
2024-01-10 19:03:26 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,27 @@ if (num === 1){
 }
 
 alert(outPut)
+
+const userInput = prompt('Введіть число');
+
+
+let num = parseInt(userInput, 10);
+let outPut;
+
+
+if (isNaN(num)) {
+    alert('Будь ласка, введіть коректне число');
+} else if (num % 100 >= 11 && num % 100 <= 14) {
+    outPut = `${num} років`;
+} else if (num % 10 === 1) {
+    outPut = `${num} рік`;
+} else if (num % 10 >= 2 && num % 10 <= 4) {
+    outPut = `${num} роки`;
+} else {
+    outPut = `${num} років`;
+}
+
+
+if (outPut) {
+    alert(outPut);
+}
