e291f95ad130d4e7953f5f1ab3f4fc9ff9ae9de4
litu3000 <acersytec@i.ua>
2024-01-12 00:32:15 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let num = null
+for (let i = 20; i <= 30; i++){
+    num = num + (i + 0.5) + ' '
+}
+console.log(num)
+
+
+let dollar = [10,20,30,40,50,60,70,80,90,100]
+let price = null
+for (let i = 0;i < dollar.length;i++){
+
+    price = price + (dollar[i] * 27) + ' '
+
+}
+console.log(price)
+
