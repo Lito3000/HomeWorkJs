49312a1bb406c2eda3ed23ec05a3105576610300
litu3000 <acersytec@i.ua>
2024-01-12 00:10:00 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,9 @@
 'use strict';
 
 
-const  userInput = prompt('Введте число')
+const  num = prompt('Введте число')
 
-let num = null;
-if (typeof userInput === 'string'){
-    num = parseInt(userInput)
-}else {
-    console.log('null',userInput)
-}
-
-
-let arr = [2,3,4]
-let outPut = null;
-
-if (num === 1){
-    outPut = `${num}` + ' ' + 'год'
-}else if (arr.includes(num)){
-    outPut = `${num}` + ' ' + 'годa'
-}else {
-    outPut = `${num}` + ' ' + 'лет'
-}
-
-alert(outPut)
-
-
-const userInput = prompt('Введіть число');
-
-
-let num = parseInt(userInput, 10);
-let outPut;
-
+let outPut = null
 
 if (isNaN(num)) {
     alert('Будь ласка, введіть коректне число');
