d2b19f94b11887cd9d421869b61f8555743b60b2
litu3000 <acersytec@i.ua>
2024-01-12 21:26:51 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
 'use strict';
 
+// Task#1
 let num = null
 for (let i = 20; i <= 30; i++){
     num = num + (i + 0.5) + ' '
 }
 console.log(num)
 
-
+// Task#2
 let dollar = [10,20,30,40,50,60,70,80,90,100]
 let price = null
 for (let i = 0;i < dollar.length;i++){
@@ -16,3 +17,34 @@ for (let i = 0;i < dollar.length;i++){
 }
 console.log(price)
 
+// Task#3
+const n = Number(prompt('Введiть число'))
+
+for (let i = 0; i < 100; i++) {
+    if (Math.pow(i, 2) > n){
+        continue;
+    }
+    else if (Math.pow(i, 2) < n) {
+        console.log(i);
+    }
+}
+
+
+// Task#4
+let number = 10;
+nextPrime:
+for (let i = 2; i <= number; i++) {
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) continue nextPrime;
+    }
+    console.log(i);
+}
+
+// Task#5
+
+const numb = Number(prompt('Введiть число'))
+let sum = numb;
+while (sum % 3 === 0) {
+    sum = sum / 3;
+}
+console.log((sum === 1)? "Число можно получить " : "Число нельзя получить");
