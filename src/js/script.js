5a1cfb6ada0fc6b4ce0ddeee72f98007452ba811
litu3000 <acersytec@i.ua>
2024-01-13 22:20:32 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
 'use strict';
 
 
-function padString() {
-    
+function padString(str, numb) {
+    if (typeof str !==  'string') {
+        return console.log('помилка строки')
+    } else if (isNaN(numb)) {
+        return console.log('помилка числа')
+    } else {
+        let star = ''
+        let g = str.length
+        let k = ''
+        for (let i = 0; i < numb; i++) {
+            star = star + '*'
+            k = star.substring(g)
+        }
+        console.log(str + k)
+    }
 }
+padString('привіт', 8)
