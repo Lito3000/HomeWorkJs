2f47198a41f9f26a3ab3ef655dd7c7ead3b869fd
litu3000 <acersytec@i.ua>
2024-01-13 20:19:38 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 
-
-
-const  elem2 = Number(prompt('Введите число'))
-if (elem2 !== 1) {
-for (let i = 2; i < elem2;i++){
-        if (elem2 % i === 0){
+const elem2 = Number(prompt('Введите число'))
+if (elem2 <= 1) {
+    console.log(NaN)
+} else {
+    for (let i = 2; i < elem2; i++) {
+        if (elem2 % i === 0) {
             console.log(i)
             break
         }
     }
-} else console.log(NaN)
+}
