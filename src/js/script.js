bdb692cc37c3f6cf339347be6addbdebb2f0638d
litu3000 <acersytec@i.ua>
2024-01-13 17:50:13 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const numb = Number(prompt('Введiть число'))
+const math = Number(prompt('Введiть ступiнь'))
+
+function primality(numb, math = 1) {
+    if (isNaN(numb)) return false
+    return numb ** math
+}
+
+let h = primality(numb, math)
+alert(h)
