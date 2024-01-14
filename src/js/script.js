8a4847e7414ccaf51bbc341fc277de3f61d187bb
litu3000 <acersytec@i.ua>
2024-01-15 01:25:43 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,31 @@
 'use strict';
 
 
-function padString(str, numb) {
-    if (typeof str !==  'string')
-        return console.log('помилка строки')
-    if (typeof year2 !== 'number')
-        return console.log('помилка числа')
+function padString(str, numb,symbol,right = true) {
+    if (typeof str !==  'string') return console.log('помилка строки');
+    if (typeof numb !== "number" || isNaN(numb)) return console.log('помилка числа');
+    if(str.length === numb) return str;
+    if(str.length > numb) return str.substring(0,numb)
+    if(typeof symbol !== 'string' || symbol.length !== 1) return console.log('Symbol length -1')
+    if(typeof right !== 'boolean' ) return console.log('Right should be Boolean')
 
         let star = ''
         let lengthStr = str.length
         let answerStar = ''
         for (let i = 0; i < numb; i++) {
-            star = star + '*'
+            star = star + symbol
             answerStar = star.substring(lengthStr)
         }
         console.log(str + answerStar)
-
+return right ? str + answerStar:answerStar +str
 }
-padString('привіт', 8)
+
+let f = padString('привіт', 8)
+let u = padString('привіт', 8,'*',false)
+let g = padString('привіт', 8,'*')
+
+console.log(u,g,f)
+
 
 // var year = prompt('Write a number');
 // if (year.replace(/\s/g, '').length === 0 || isNaN(year)) {
@@ -26,7 +34,7 @@ padString('привіт', 8)
 //     alert('Вы полностью правы!');
 // }
 //
-var year2 = 5;
+// var year2 = 5;
 // console.log(year2)
 // console.log(typeof year2)
-// console.log(Boolean(' '))
+// console.log(Boolean(5))
