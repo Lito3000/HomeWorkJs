869cc45544404619563dfebad89ef440fd5a9aa1
litu3000 <acersytec@i.ua>
2024-01-15 16:22:04 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let arr = [1, 2, 3, -1, -2, -3];
+const recycledArray = function recycle(arr) {
+    if ((arr.length <= 0) || (!Array.isArray(arr))) return 'array is empty'
+    const positiveArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positiveArr.push(arr[i])
+        }
+    }
+    if ((positiveArr.length <= 0) || (!Array.isArray(positiveArr))) {
+        return null
+    } else {
+        return positiveArr
+    }
+}
+console.log(recycledArray(arr))
