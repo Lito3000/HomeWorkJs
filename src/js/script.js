3779066b64fa3d24c9bff27abc01145148df9d7c
litu3000 <acersytec@i.ua>
2024-01-16 15:26:47 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const creatFunction = function (elem,str) {
+
+    for (let i = 0; i < elem; i++) {
+
+        sum = sum + str[Math.round(Math.random() * (str.length - 1))];
+    }
+}
+let sum = '';
+creatFunction(16,characters)
+console.log(sum)
