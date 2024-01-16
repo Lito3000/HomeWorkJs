4d4664def3165de89ee8a95234f32b147780a7a9
litu3000 <acersytec@i.ua>
2024-01-16 17:50:30 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,37 @@
 'use strict';
 
-const sum = null;
-const creatFunction = function (sum, max) {
+// const sum = null;
+// const creatFunction = function (sum, max) {
+//
+//     for (let i = 0; i < max; i++) {
+//
+//         sum = sum + (Math.round(Math.random() * 100)) + ' ';
+//     }
+//     return sum
+// }
+//
+// const result = creatFunction(sum, 100)
+// console.log(result)
 
-    for (let i = 0; i < max; i++) {
-
-        sum = sum + (Math.round(Math.random() * 100)) + ' ';
-    }
-    return sum
+Date.prototype.getWeek = function() {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+        - 3 + (week1.getDay() + 6) % 7) / 7);
 }
 
-const result = creatFunction(sum, 100)
-console.log(result)
+// Returns the four-digit year corresponding to the ISO week of the date.
+Date.prototype.getWeekYear = function() {
+    var date = new Date(this.getTime());
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    return date.getFullYear();
+
+}
+
+let g = Date.prototype.getWeekYear
+console.log(g)
