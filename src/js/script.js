658e81c56c8522e6781873d6eeae15c124ab2802
litu3000 <acersytec@i.ua>
2024-01-17 09:25:54 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const creatFunction = function (max) {
-
     for (let i = 0; i < max; i++) {
-
-        sum1.push(Math.round(Math.random() * 100));
+        sum.push(Math.round(Math.random() * 100));
     }
 }
-let sum1 = []
+let sum = []
 creatFunction(100)
-console.log(sum1)
+console.log(sum)
