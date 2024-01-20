a2a01b3e5ecdd379352f789d028b809543d80c6a
litu3000 <acersytec@i.ua>
2024-01-20 22:38:01 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const cookRoom = Number(prompt('How many liters of borscht to prepare?'))
-let kitchen = function kitche(kilo) {
+const kitchen = function kitche(kilo) {
     if (isNaN(kilo) || !(typeof kilo === "number")) return NaN;
 
     return Math.round(4 * 0.75 * kilo) + 'кг' + ' ' + (4 * 0.75 * kilo * 13) + 'грн';
