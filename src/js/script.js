8ea4eb907079b176d8ad5db8008ef8a20d62c6d0
litu3000 <acersytec@i.ua>
2024-01-21 11:34:33 +0200

    initial

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const arr = [1, 2, 3, -1, -2, -3];
+const arr = [1, 2, 3, -1,0, -2, -3];
 const recycledArray = function recycle(arr) {
     if ((arr.length <= 0) || (!Array.isArray(arr))) return 'array is empty'
     const positiveArr = []
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) {
+        if (arr[i] >= 0) {
             positiveArr.push(arr[i])
         }
     }
