18fccfe0cebda980c8267abcb037427f1c5d1b28
litu3000 <acersytec@i.ua>
2024-01-22 02:17:29 +0200

    initial

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line func-names
+const plus = function (a, b) {
+  return a + b;
+};
+plus();
+const minus = function minus(a, b) {
+  return a - b;
+};
+minus();
+
+// eslint-disable-next-line func-names
+const multi = function (a, b) {
+  return a * b;
+};
+multi();
