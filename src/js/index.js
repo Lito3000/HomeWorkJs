9d97cd5c6ab6babc27c3c0b3edcb9dd8045a0bab
litu3000 <acersytec@i.ua>
2024-01-24 23:45:45 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,58 @@
-const createUser = function obj(a, b) {
-  const user = {
-    name: a,
-    age: b,
-  };
-  user.greet = greet;
-  user.ageUser = years;
-  return user;
-};
+// const createUser = function obj(a, b) {
+//   const user = {
+//     name: a,
+//     age: b,
+//   };
+//   user.greet = greet;
+//   user.ageUser = years;
+//   return user;
+// };
+//
+// const greet = function greet() {
+//   console.log(`Hello, my name is ${this.name}`);
+// };
+//
+// const years = function years() {
+//   console.log(`My age is ${this.age}`);
+// };
+//
+// const user1 = createUser('Alice', 25);
+// const user2 = createUser('Bob', 30);
+// user1.greet();
+// user2.greet();
+// user1.ageUser();
+// const g = user1.age;
+// console.log(g);
 
-const greet = function greet() {
-  console.log(`Hello, my name is ${this.name}`);
-};
+// function fib(n) {
+//   if (n <= 0) {
+//     return 0;
+//   }
+//   if (n <= 2) {
+//     return 1;
+//   }
+//   const l = fib(n - 1);
+//   console.log(l);
+//   const k = fib(n - 2);
+//   console.log(k);
+//   const y = fib(n - 1) + fib(n - 2);
+//   console.log(y);
+//   return fib(n - 1) + fib(n - 2);
+// }
+// const g = fib(3);
+// console.log(g);
 
-const years = function years() {
-  console.log(`My age is ${this.age}`);
-};
+function nachi(num) {
+  if (num < 2) {
+    return num;
+  }
+  const l = nachi(num - 1);
+  console.log(l);
+  const k = nachi(num - 2);
+  console.log(k);
+  const y = nachi(num - 1) + nachi(num - 2);
+  console.log(y);
+  return nachi(num - 1) + nachi(num - 2);
+}
 
-const user1 = createUser('Alice', 25);
-const user2 = createUser('Bob', 30);
-user1.greet();
-user2.greet();
-user1.ageUser();
-const g = user1.age;
-console.log(g);
+console.log(nachi(2));
