5f6d51e61b11ee2400443509d5fc2132d9151b49
litu3000 <acersytec@i.ua>
2024-01-29 22:03:43 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,71 @@
-const createUser = function obj(name, skills) {
-  const user = {
+const skillsRequirements = ['HTML', 'CSS', 'JS', 'React', 'Vue', 'NodeJS'];
+const skillsList = ['HTML', 'CSS', 'JS'];
+
+const createCandidate = function (name = 'Name not provided', skills = []) {
+  return {
     name,
-    skills,
-  };
+    skills
+  }
+}
 
-  // eslint-disable-next-line no-use-before-define
-  user.req = requirement;
-  // user.ageUser = years;
-  return user;
-};
+// console.log(createCandidate('Jon Doe', skillsList));
 
-const requirement = function req() {
-  console.log(`Hello, my name is ${this.name}`);
-};
 
-// const years = function years() {
-//   console.log(`My age is ${this.age}`);
-// };
+const addRequirement = function (skill, listOfRequirements ) {
+  listOfRequirements.push(skill);
+  return listOfRequirements
+}
 
-const user1 = createUser('Alice', 25, 6);
-const user2 = createUser('Bob', 30);
-console.log(user1.skills);
-console.log(user2.name);
-user2.req();
+addRequirement('Angular', skillsRequirements);
+
+// console.log(skillsRequirements)
+
+const interview = function (candidate, requirements) {
+  let result = null;
+
+
+  for(let i = 0; i < candidate.skills.length; i++) {
+    // console.log(candidate.skills[i]);
+
+    for(let r = 0; r < requirements.length; r++) {
+      if(candidate.skills[i] === requirements[r]){
+        result = 100;
+      }
+      else {
+        result = 0;
+      }
+
+    }
+  }
+
+  return result;
+}
+
+// console.log(interview)
+
+
+const planInterviews = function (candidates, req, scoreFunc) {
+  const result = [];
+  for(let i = 0; i < candidates.length; i++) {
+    //1 Погано
+    // candidates[i].score = scoreFunc(candidates[i], req)
+    // result.push(candidates[i])
+
+    //2 добре
+    result.push({
+      score:  scoreFunc(candidates[i], req),
+      candidate: candidates[i]
+    })
+  }
+
+  return result;
+}
+
+
+const candidate1 = createCandidate('Dog', skillsList);
+const candidate2 = createCandidate('Cat', skillsList);
+const candidate3 = createCandidate('Vlad', skillsList);
+const candidates = [candidate1, candidate2, candidate3];
+
+
+console.log(planInterviews(candidates, skillsRequirements, interview))
