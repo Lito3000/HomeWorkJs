f85bd97c32d4db6dd4ea2f47ec1b00ce6b6bfe93
litu3000 <acersytec@i.ua>
2024-01-30 01:25:36 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
 const skillsRequirements = ['HTML', 'CSS', 'JS', 'React', 'Vue', 'NodeJS'];
-const skillsList = ['HTML', 'CSS', 'JS'];
-
+const skillsList1 = ['HTML', 'CSS', 'JS'];
+const skillsList2 = ['HTML', 'CSS', 'JS', 'React', 'Vue', 'NodeJS', 'Angular'];
+const skillsList3 = ['HTML', 'CSS', 'JS'];
 const createCandidate = function (name = 'Name not provided', skills = []) {
   return {
     name,
-    skills
-  }
-}
+    skills,
+  };
+};
 
 // console.log(createCandidate('Jon Doe', skillsList));
 
-
-const addRequirement = function (skill, listOfRequirements ) {
+const addRequirement = function (skill, listOfRequirements) {
   listOfRequirements.push(skill);
-  return listOfRequirements
-}
+  return listOfRequirements;
+};
 
 addRequirement('Angular', skillsRequirements);
 
@@ -23,49 +23,47 @@ addRequirement('Angular', skillsRequirements);
 const interview = function (candidate, requirements) {
   let result = null;
 
-
-  for(let i = 0; i < candidate.skills.length; i++) {
+  for (let i = 0; i < candidate.skills.length; i++) {
     // console.log(candidate.skills[i]);
 
-    for(let r = 0; r < requirements.length; r++) {
-      if(candidate.skills[i] === requirements[r]){
+    for (let r = 0; r < requirements.length; r++) {
+      if (candidate.skills[i] === requirements[r]) {
         result = 100;
-      }
-      else {
+      } else {
         result = 0;
       }
-
     }
   }
 
   return result;
-}
+};
 
 // console.log(interview)
 
-
 const planInterviews = function (candidates, req, scoreFunc) {
   const result = [];
-  for(let i = 0; i < candidates.length; i++) {
-    //1 Погано
+  for (let i = 0; i < candidates.length; i++) {
+    // 1 Погано
     // candidates[i].score = scoreFunc(candidates[i], req)
     // result.push(candidates[i])
 
-    //2 добре
+    // 2 добре
     result.push({
-      score:  scoreFunc(candidates[i], req),
-      candidate: candidates[i]
-    })
+      score: scoreFunc(candidates[i], req),
+      candidate: candidates[i],
+      pads: req,
+    });
   }
 
   return result;
-}
+};
 
-
-const candidate1 = createCandidate('Dog', skillsList);
-const candidate2 = createCandidate('Cat', skillsList);
-const candidate3 = createCandidate('Vlad', skillsList);
+const candidate1 = createCandidate('Dog', skillsList1);
+const candidate2 = createCandidate('Cat', skillsList2);
+const candidate3 = createCandidate('Vlad', skillsList3);
 const candidates = [candidate1, candidate2, candidate3];
 
-
-console.log(planInterviews(candidates, skillsRequirements, interview))
+const r = interview(candidate2, skillsRequirements);
+console.log(r);
+const k = planInterviews(candidates, skillsRequirements, interview);
+console.log(k);
