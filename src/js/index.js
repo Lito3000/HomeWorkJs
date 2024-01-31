2edb4ba800147685cf8d4b8a3f37c4853383792e
litu3000 <acersytec@i.ua>
2024-01-31 19:10:15 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,16 @@
-function updateQueryString(url, key, value) {
+const upd = function updateQueryString(url, key, value) {
   const parsedUrl = new URL(url);
-  parsedUrl.searchParams.set(key, value);
-  parsedUrl.searchParams.get('s', '56');
+  parsedUrl.searchParams.get(key, value);
+  parsedUrl.searchParams.set('s', '67');
 
   return parsedUrl.toString();
-}
+};
 
 // Приклад використання
-console.log(updateQueryString('https://example.com/path?query=123', 'query', '456'));
+const g = upd('https://example.com/path?query=123', 'query', '456');
+console.log(g);
 
-const params = new URL(document.location).searchParams;
-const name = params.get('name'); // is the string "Jonathan Smith".
-const age = parseInt(params.get('age')); // is the number 18
-console.log(age);
-
-const paramsString = 'https://example.com/search?q=query&sort=ascending&page=2';
+const paramsString = g;
 const searchParams = new URLSearchParams(paramsString);
 
 // Iterating the search parameters
@@ -22,14 +18,14 @@ for (const p of searchParams) {
   console.log(p);
 }
 
-console.log(searchParams.has('topic')); // true
-console.log(searchParams.has('topic', 'fish')); // false
-console.log(searchParams.get('topic') === 'api'); // true
-console.log(searchParams.getAll('topic')); // ["api"]
-console.log(searchParams.get('foo') === null); // true
-console.log(searchParams.append('topic', 'webdev'));
-console.log(searchParams.toString()); // "q=URLUtils.searchParams&topic=api&topic=webdev"
-console.log(searchParams.set('topic', 'More webdev'));
-console.log(searchParams.toString()); // "q=URLUtils.searchParams&topic=More+webdev"
-console.log(searchParams.delete('topic'));
-console.log(searchParams.toString()); // "q=URLUtils.searchParams"
+// console.log(searchParams.has('topic')); // true
+// console.log(searchParams.has('topic', 'fish')); // false
+// console.log(searchParams.get('topic') === 'api'); // true
+// console.log(searchParams.getAll('topic')); // ["api"]
+// console.log(searchParams.get('foo') === null); // true
+// console.log(searchParams.append('topic', 'webdev'));
+// console.log(searchParams.toString()); // "q=URLUtils.searchParams&topic=api&topic=webdev"
+// console.log(searchParams.set('topic', 'More webdev'));
+// console.log(searchParams.toString()); // "q=URLUtils.searchParams&topic=More+webdev"
+// console.log(searchParams.delete('topic'));
+// console.log(searchParams.toString()); // "q=URLUtils.searchParams"
