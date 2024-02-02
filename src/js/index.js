5e40fc831a85fcfde7c03cd2bf109c8cb73e124b
litu3000 <acersytec@i.ua>
2024-02-02 21:37:42 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,9 @@
 const upd = function updateQueryString(url, key, value) {
   const parsedUrl = new URL(url);
-  parsedUrl.searchParams.get(key, value);
-  parsedUrl.searchParams.set('s', '67');
+  parsedUrl.searchParams.set(key, value);
 
   return parsedUrl.toString();
 };
 
-// Приклад використання
 const g = upd('https://example.com/path?query=123', 'query', '456');
 console.log(g);
-
-const paramsString = g;
-const searchParams = new URLSearchParams(paramsString);
-
-// Iterating the search parameters
-for (const p of searchParams) {
-  console.log(p);
-}
-
-// console.log(searchParams.has('topic')); // true
-// console.log(searchParams.has('topic', 'fish')); // false
-// console.log(searchParams.get('topic') === 'api'); // true
-// console.log(searchParams.getAll('topic')); // ["api"]
-// console.log(searchParams.get('foo') === null); // true
-// console.log(searchParams.append('topic', 'webdev'));
-// console.log(searchParams.toString()); // "q=URLUtils.searchParams&topic=api&topic=webdev"
-// console.log(searchParams.set('topic', 'More webdev'));
-// console.log(searchParams.toString()); // "q=URLUtils.searchParams&topic=More+webdev"
-// console.log(searchParams.delete('topic'));
-// console.log(searchParams.toString()); // "q=URLUtils.searchParams"
