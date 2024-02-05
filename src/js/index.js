430ac5412c58a0c90a0f0a55c6c0021dc85757bb
litu3000 <acersytec@i.ua>
2024-02-05 14:44:25 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -21,8 +21,8 @@
 // forEac(arr,log)
 // forEac(arr,getLength)
 // forEac(arr,toUp)
-
-
+//
+//
 //--------------------------------------------------------------------------------------------------------
 // const massiv = ['car','ship','aircraft']
 //
@@ -70,7 +70,8 @@
 // const hidenHello = function (str){
 //   console.log(str)
 // }
-// //вызывается функция greet вместо аргумента hello подставляется аргумент seyHifunc('Hi!')
+// //вызывается функция greet вместо аргумента hello подставляется аргумент seyHifunc('Hi!') вызывается функция
+// // hello
 // greet(hello)//вызывается функция greet вместо аргумента hello подставляется аргумент str  функции hidenHello
 // greet(hidenHello)
 //-------------------------------------------------------------------------------------------------------
@@ -113,7 +114,8 @@
 // const log = function (element,item){//передадим сюда data[i]  c callback функции
 //     console.log(element,item)
 // }
-// //вызывается функция forEach вместо аргумента log подставляется аргумент callback(data[i],i,data)
+// //вызывается функция forEach вместо аргумента log подставляется аргумент callback(data[i],i,data) и
+// // данные передаются в функцию log функция log дает ответ
 // forEach(arr,log)//вызывается функция forEach вместо аргумента log подставляется аргумент element  функции log
 //                 //аргумент(element) callback функции log передастся функции forEach
 //--------------------------------------------------------------------------------------------------------
@@ -155,32 +157,143 @@ const arr3 = ['cotton', 'pads', 'natures', 'touch', 'softest'];
 // console.log(arr3)
 // console.log(length)
 
+//
+// const map3 = (data, callback) => {
+//     const result = []
+//     for (let i = 0; i < data.length; i++) {
+//         result.push(
+//             callback(data[i], i, data)
+//         )
+//     }
+//
+//     return result
+// }
+//
+// const log = function (elemen, index, arr) {
+//     console.log(elemen)
+//     return elemen
+//
+// }
+// const logIndex = function (elemen, index, arr) {
+//     console.log(index)
+//     return index
+//
+// }
+//
+// let r = map3(arr3, log)
+// console.log(r)
+// let h = map3(arr3, logIndex)
+// console.log(h)
 
-const map3 = (data, callback) => {
-    const result = []
-    for (let i = 0; i < data.length; i++) {
-        result.push(
-            callback(data[i], i, data)
-        )
+//------------------------------------------------------------------------------------------------------------
+
+// const raid = [3,8,9,5,1]
+// const id = 8
+// const myFind = function (arr, callback) {
+//     for (let i = 0; i < arr.length; i++) {
+//         const result = callback(arr[i])
+//         if (result) {
+//             return arr[i]
+//         }
+//     }
+//     return 'no result'
+// }
+//
+// const cb = function (item){
+//     if(item === id){
+//         return true
+//     }
+// }
+//
+//
+// const result = myFind(raid,cb)
+// console.log(result)
+//
+//
+//----------------------------------------------------------------------------------------------------------
+// const raid = [3,8,9,5,1]
+// const id = 1
+// const myIncludes = function (arr, callback) {
+//     for (let i = 0; i < arr.length; i++) {
+//         const result = callback(arr[i])
+//         if (result) {
+//             return true
+//         }
+//     }
+//     return false
+// }
+//
+// const cb = function (it){
+//     if(it === id){
+//         return true
+//     }
+// }
+//
+//
+// const result = myIncludes(raid,cb)
+// console.log(result)
+//
+//
+//---------------------------------------------------------------------------------------------------------
+// const raid = [3,8,9,5,1]
+// const id = 5
+// const myIndexof = function (arr, callback) {
+//     for (let i = 0; i < arr.length; i++) {
+//         const result = callback(arr[i])
+//         if (result) {
+//             return i
+//         }
+//     }
+//     return 'no result'
+// }
+//
+// const cb = function (item){
+//     if(item === id){
+//         return true
+//     }
+// }
+//
+//
+// const result = myIndexof(raid,cb)
+// console.log(result)
+//
+//
+//----------------------------------------------------------------------------------------------------------
+const raid = [3,8,9,5,1]
+// const id = 5
+const myIndexof = function (callback,arr,index) {
+    for (let i = 0; i < arr.length; i++) {
+        const result = callback(arr[i],index)
+        if (result) {
+            return i
+        }
     }
-
-    return result
+    return 'no result'
 }
 
-const log = function (elemen, index, arr) {
-    console.log(elemen)
-    return elemen
-
-}
-const logIndex = function (elemen, index, arr) {
-    console.log(index)
-    return index
-
+const cb = function (item,id){
+    if(item === id){
+        return true
+    }
 }
 
-let r = map3(arr3, log)
-console.log(r)
-let h = map3(arr3, logIndex)
-console.log(h)
+
+const result = myIndexof(cb,raid,8)
+console.log(result)
+const result2 = myIndexof(cb,raid,7)
+console.log(result2)
 
 
+
+const raid = [3, 8, 9, 5, 1]
+const myIndexof = function (arr, number, index = raid.length - 1) {
+    for (let i = index; i < arr.length; i = i - 1) {
+        if (arr[i] === number) {
+            return i
+        }
+    }
+    return 'no result'
+}
+
+const result = myIndexof(raid, 1)
+console.log(result)
