921c7928aac2c731cfcf47dbde129e6039fdce3c
litu3000 <acersytec@i.ua>
2024-02-08 23:24:01 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,40 @@
 // Task#1
-function runCallbacks(callbacks, value) {
-    return callbacks.reduce((accumulator, currentCallback) => currentCallback(accumulator), value);
+// function runCallbacks(callbacks, value) {
+//     return callbacks.reduce((accumulator, currentCallback) => currentCallback(accumulator), value);
+// }
+// const addOne = x => x + 1;
+// const square = x => x * x;
+// const callbacks = [addOne, square, addOne];
+// const result = runCallbacks(callbacks, 1);
+// console.log(result);
+
+
+// (function(){
+//     // return 10 + 20
+//     // 'use strict'
+//     // let f = function example(){
+//     //     return 10 + 20;
+//     // }
+//     //
+//     // console.log(f())
+//     console.log(100)
+//     var g = 'volvo'
+//     let h = 100
+//     function go (){
+//         console.log('vova')
+//     }
+//     go()
+//     console.log(h)
+//     console.log(g)
+// })
+// ()
+
+if(true){
+    var foo = 285;
+    const g = function f() {
+        console.log('vire')
+    }
 }
-const addOne = x => x + 1;
-const square = x => x * x;
-const callbacks = [addOne, square, addOne];
-const result = runCallbacks(callbacks, 1);
-console.log(result);
+console.log(foo)
+f()
 
