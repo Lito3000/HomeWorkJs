db87016548455f498718e59c957e6d35142f40f4
litu3000 <acersytec@i.ua>
2024-02-10 23:29:16 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,10 @@
-
-Array.prototype.myReduce = function (fn, acc) {
-    for (let i = 0; i < this.length; i++) {
-        acc = fn(acc, this[i], i, this);
+// Task#1
+const runCallbacks = function(callback, index) {
+    let result = index
+    for (let i = 0; i < callback.length; i++) {
+        result = callback[i](result)
     }
-    return acc
-};
-function runCallbacks(backs, value) {
-    return backs.myReduce(function (accumulator, currentCallback) {
-        return  currentCallback(accumulator)
-    }, value);
+    return result
 }
 const addOne = x => x + 1;
 const square1 = x => x * x;
@@ -16,115 +12,20 @@ const callbacks = [addOne, square1, addOne];
 const result = runCallbacks(callbacks, 1);
 console.log(result);
 
-const  raid = [6,8,5,3,9]
-const myFindIndex = function (arr, callback, index) {
-    for (let i = 0; i < arr.length; i++) {
+//Task#2
 
-        if (callback(arr[i], index, arr)) {
-            return i
+function composeFunctions(...funcs) {
+    return function (value) {
+        let result = value
+        for (let i = funcs.length - 1; i >= 0; i--) {
+            result =  funcs[i](result)
         }
-    }
-    return -1
-}
-
-const result4 = myFindIndex(raid, function (item, id) {
-    if (item === id) {
-        return true
-    }
-}, 1)
-console.log(result4)
-function composeFunctions(...funcs){
-    console.log(funcs)
-    return function (value){
-        return funcs.reduceRight(function(currentValue,currentFunc){
-            return currentFunc(currentValue)
-        },value);
+        return result
     }
 }
-
 
 const double = x => x * 2;
 const square = x => x * x;
 const negate = x => -x;
 const composedFunction = composeFunctions(double, square, negate);
 console.log(composedFunction(2)); // Виведе: -16 (негація квадрату подвоєного 2: -((2*2)^2))
-
-
-// function composeFunctions(...funcs){
-//     console.log(funcs)
-//     return function (value){
-//         return funcs.reduceRight(function(currentValue,currentFunc){
-//             return currentFunc(currentValue)
-//         },value);
-//     }
-// }
-//
-//
-// const double = x => x * 2;
-// const square = x => x * x;
-// const negate = x => -x;
-// const composedFunction = composeFunctions(double, square, negate);
-// console.log(composedFunction(2)); // Виведе: -16 (негація квадрату подвоєного 2: -((2*2)^2))
-
-
-// Task#1
-// function runCallbacks(callbacks, value) {
-//     return callbacks.reduce((accumulator, currentCallback) => currentCallback(accumulator), value);
-// }
-// const addOne = x => x + 1;
-// const square = x => x * x;
-// const callbacks = [addOne, square, addOne];
-// const result = runCallbacks(callbacks, 1);
-// console.log(result);
-
-
-
-// const runCallbacks = function(callback, index) {
-//     let result = index;
-//     for (let i = 0; i < callback.length; i++) {
-//         result = callback[i](result)
-//     }
-//     return result
-// }
-// const addOne = x => x + 1;
-// const square = x => x * x;
-// const callbacks = [addOne, square, addOne];
-// const result = runCallbacks(callbacks, 1);
-// console.log(result);
-//----------------------------------------------------------------------------------------------------------
-// (function(){
-//     // return 10 + 20
-//     // 'use strict'
-//     // let f = function example(){
-//     //     return 10 + 20;
-//     // }
-//     //
-//     // console.log(f())
-//     console.log(100)
-//     var g = 'volvo'
-//     let h = 100
-//     function go (){
-//         console.log('vova')
-//     }
-//     go()
-//     console.log(h)
-//     console.log(g)
-// })
-// ()
-
-if(true){
-
-   function f() {
-        console.log('vire')
-    }
-
-
-    let g = 6
-    var foo = 285;
-
-}
-f()
-// console.log(g)
-console.log(foo)
-
-
