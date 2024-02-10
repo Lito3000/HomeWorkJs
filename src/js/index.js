f5837b36f5d0f00e68f6c474d357acfe8b1b16ef
litu3000 <acersytec@i.ua>
2024-02-11 00:04:44 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,47 @@
 // Task#1
-const runCallbacks = function(callback, index) {
-    let result = index
-    for (let i = 0; i < callback.length; i++) {
-        result = callback[i](result)
-    }
-    return result
-}
-const addOne = x => x + 1;
-const square1 = x => x * x;
-const callbacks = [addOne, square1, addOne];
-const result = runCallbacks(callbacks, 1);
-console.log(result);
+// const runCallbacks = function(callback, index) {
+//     let result = index
+//     for (let i = 0; i < callback.length; i++) {
+//         result = callback[i](result)
+//     }
+//     return result
+// }
+// const addOne = x => x + 1;
+// const square1 = x => x * x;
+// const callbacks = [addOne, square1, addOne];
+// const result = runCallbacks(callbacks, 1);
+// console.log(result);
+//
+// //Task#2
+//
+// function composeFunctions(...funcs) {
+//     return function (value) {
+//         let result = value
+//         for (let i = funcs.length - 1; i >= 0; i--) {
+//             result =  funcs[i](result)
+//         }
+//         return result
+//     }
+// }
+//
+// const double = x => x * 2;
+// const square = x => x * x;
+// const negate = x => -x;
+// const composedFunction = composeFunctions(double, square, negate);
+// console.log(composedFunction(2)); // Виведе: -16 (негація квадрату подвоєного 2: -((2*2)^2))
 
-//Task#2
 
-function composeFunctions(...funcs) {
-    return function (value) {
-        let result = value
-        for (let i = funcs.length - 1; i >= 0; i--) {
-            result =  funcs[i](result)
-        }
-        return result
+function composeFunctions(...funcs){
+    console.log(funcs)
+    return function (value){
+        return funcs.reduceRight(function(currentValue,currentFunc){
+            return currentFunc(currentValue)
+        },value);
     }
 }
 
 const double = x => x * 2;
 const square = x => x * x;
 const negate = x => -x;
-const composedFunction = composeFunctions(double, square, negate);
-console.log(composedFunction(2)); // Виведе: -16 (негація квадрату подвоєного 2: -((2*2)^2))
+const composedFun = composeFunctions(double, square, negate);
+console.log(composedFun(2)); // Виведе: -16 (негація квадрату подвоєного 2: -((2*2)^2))
