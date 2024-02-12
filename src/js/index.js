5e09137987fe774548666ccdf13b66935cdc3962
litu3000 <acersytec@i.ua>
2024-02-12 12:45:15 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,10 @@
 
-//Task#1 Нахождение максимального числа в масиве
+//Task#1 Сумма элементов массива
 const elem = [8,7,0,3,5,1]
 
-const maxNumber = elem.reduce(function (total, number, index, array) {
-  if (total < number) {
-    return number;
-  } else {
-    return total
-  }
-}, 0)
+const sumFunction = elem.reduce(function (accum,item){
+  accum = accum + item
+return accum
+},0)
 
-console.log(maxNumber)
+console.log(sumFunction)
