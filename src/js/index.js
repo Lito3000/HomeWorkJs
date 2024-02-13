9c601b8280310ff36fff198a3ddd2236c6ca7d64
litu3000 <acersytec@i.ua>
2024-02-13 20:01:45 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,17 @@
 //Task#1 Сумма элементов массива
 const elem = [8,7,0,3,5,1]
 
-const sumFunction = elem.reduce(function (accum,item){
-  accum = accum + item
-return accum
-},0)
+const myReduce = function (fn,acc) {
+    for (let i = 0; i < elem.length; i++) {
+        acc = fn(acc, elem[i], i, elem);
+    }
+    return acc
+};
+
+const sumFunction = myReduce(function (total, number, index) {
+    console.log(`Index: ${index} Total: ${total} Number: ${number} Array: ${elem}`)
+    return total + number
+}, 0)
 
 console.log(sumFunction)
+
