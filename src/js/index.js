038730b3d24d641fb32aba3320c6a2a579d297a5
litu3000 <acersytec@i.ua>
2024-02-16 01:06:43 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,21 @@
-function factorial4(num, degree) {
+//Task#1
+function getFactorial(n){
+    if (n === 0) {
+        return 1
+    }else {
+        return n * getFactorial(n - 1)
+    }
+}
+console.log(getFactorial(4))
+
+//Task#2
+function pow(num, degree) {
     if (degree === 0) {
         return 1
     } else {
-        return num * factorial3(num, degree - 1)
+        return num * pow(num, degree - 1)
     }
 }
 
-function factorial3(num,degree) {
-    if (degree === 0) {
-        return 1
-    } else {
-        return num * factorial2(num, degree - 1)
-    }
-}
+console.log(pow(2, 12))
 
-function factorial2(num,degree) {
-    if (degree === 0) {
-        return 1
-    } else {
-        return num * factorial1(num, degree - 1)
-    }
-}
-
-function factorial1(num,degree) {
-    if (degree === 0) {
-        return 1
-    } else {
-        return num * factorial0(num, degree - 1)
-    }
-}
-
-function factorial0(num,degree) {
-    if (degree === 0) {
-        return 1
-    } else {
-        return num * factorial4(num, degree - 1)
-    }
-}
-
-//pow(num, degree)
-console.log(factorial4(2, 12))
-
-
-// function g(5+7){
-// g(6)
-// }
-//
-// console.log( g(5+5))
-// console.log(4 + g(5))
-// console.log(4 + undefined)
