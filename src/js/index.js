20420301516aa8a4966ac990e1240385042af16b
litu3000 <acersytec@i.ua>
2024-02-16 00:52:07 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,45 @@
-function factorial4(n) {
-    if (n === 0) {
+function factorial4(num, degree) {
+    if (degree === 0) {
         return 1
     } else {
-        return n * factorial3(n - 1)
-    }
-}
-function factorial3(n) {
-    if (n === 0) {
-        return 1
-    } else {
-        return n * factorial2(n - 1)
-    }
-}
-function factorial2(n) {
-    if (n === 0) {
-        return 1
-    } else {
-        return n * factorial1(n - 1)
-    }
-}
-function factorial1(n) {
-    if (n === 0) {
-        return 1
-    } else {
-        return n * factorial0(n - 1)
-    }
-}
-function factorial0(n) {
-    if (n === 0) {
-        return 1
-    }else {
-        return n * factorial0(n - 1)
+        return num * factorial3(num, degree - 1)
     }
 }
 
-console.log(factorial4(4))
+function factorial3(num,degree) {
+    if (degree === 0) {
+        return 1
+    } else {
+        return num * factorial2(num, degree - 1)
+    }
+}
+
+function factorial2(num,degree) {
+    if (degree === 0) {
+        return 1
+    } else {
+        return num * factorial1(num, degree - 1)
+    }
+}
+
+function factorial1(num,degree) {
+    if (degree === 0) {
+        return 1
+    } else {
+        return num * factorial0(num, degree - 1)
+    }
+}
+
+function factorial0(num,degree) {
+    if (degree === 0) {
+        return 1
+    } else {
+        return num * factorial4(num, degree - 1)
+    }
+}
+
+//pow(num, degree)
+console.log(factorial4(2, 12))
 
 
 // function g(5+7){
