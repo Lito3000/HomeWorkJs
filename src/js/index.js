619189fd7ef1f86c70dffefd22fe973c3c8a0c1a
litu3000 <acersytec@i.ua>
2024-02-16 00:10:56 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,42 @@
-//Task #1
-
-function factorial(n) {
-
+function factorial4(n) {
     if (n === 0) {
-
         return 1
-
-    }else {
-
-       return  n * factorial(n - 1)
-
+    } else {
+        return n * factorial3(n - 1)
+    }
+}
+function factorial3(n) {
+    if (n === 0) {
+        return 1
+    } else {
+        return n * factorial2(n - 1)
+    }
+}
+function factorial2(n) {
+    if (n === 0) {
+        return 1
+    } else {
+        return n * factorial1(n - 1)
+    }
+}
+function factorial1(n) {
+    if (n === 0) {
+        return 1
+    } else {
+        return n * factorial0(n - 1)
+    }
+}
+function factorial0(n) {
+    if (n === 0) {
+        return 1
     }
 }
 
-console.log(factorial(4))
+console.log(factorial4(4))
 
 
-// const g = function (y){
-// return y
+// function g(5+7){
+// g(6)
 // }
 //
 // console.log( g(5+5))
