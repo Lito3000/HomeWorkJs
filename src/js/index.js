8266111133d8bad5a453a0ce93abe3d8ce9b6e31
litu3000 <acersytec@i.ua>
2024-02-21 00:21:43 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
+//Task#1
+const obj = {
+    name: 'Alex',
+    age: '23',
+}
+
+const elem = function (prefix) {
+    console.log(this, prefix)
+    console.log(prefix + ' ' + this.name)
+}
+
+
+function myApply(func, context, argument) {
+    context.foo = func
+    context.foo(argument)
+    delete  context.foo;
+    console.log('myAplay', context)
+}
+
+myApply(elem, obj, 'Mr.')
+
+
 
