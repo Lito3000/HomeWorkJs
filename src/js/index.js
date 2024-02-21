fbb4a374b6bab51d25bd689a718508fee14d727a
litu3000 <acersytec@i.ua>
2024-02-21 17:07:02 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
+//Task#1
 
+const arr = ['professional', 'r', 'strength', 'cleansing', 'moisturising', 'hand', 'cleanser', 'action', 'antimicrobial'];
+
+const x = (str) => {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].length > 5) {
+            str[i] = str[i]
+        } else {
+           delete str[i]
+        }
+    }
+    return arr
+}
+
+const result = x(arr)
+console.log(result)
