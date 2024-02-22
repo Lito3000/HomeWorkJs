4eb1171a0b36f90ae4bd78fa9ed6a9d0e39a20ba
litu3000 <acersytec@i.ua>
2024-02-22 13:09:05 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,16 @@ let ladder = {
     step: 0,
     up: function () {
         this.step++;
+        return this
     },
     down: function () {
         this.step--;
+        return this
     },
-    showStep: function () { // показывает текущую ступеньку
-        alert(this.step);
+    showStep: function () {
+        console.log(this.step);
+        return this
     }
 };
 
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep(); // 1
-
-// ladder.up().up().down().showStep(); // 1
+ladder.up().up().up().up().down().showStep();
