33acbcac0fd356318a0c12d4706e07f91b609837
litu3000 <acersytec@i.ua>
2024-02-27 19:37:09 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-//Task#1
+//Task#1-1
 (function () {
     'use strict'
     const resultProduct = {}
@@ -21,8 +21,26 @@
     const res = product.getFullName()
     console.log(res)
     console.log(product)
+//Task#1-2
+    const prod = {
+        flour: 56,
+        butter: 90,
+        sugar: 50,
+        eggs: 35,
+        chocolate: 20,
+    }
+    const example = (elem) => {
+      for (let key in elem){
+          elem[key] = elem[key] - (elem[key]* 10/100)
+      }
+      return elem
+    }
+    console.log(prod)
+    const prodResult = example(prod)
+    console.log(prodResult)
 
-// Task#2
+
+// Task#2-1
     const rating = {}
     const student = {
         John: 45,
@@ -46,4 +64,25 @@
     const result = student.sortRating()
     console.log(result)
     console.log(student)
+
+    //Task#2-2
+    const stud ={
+        John: 45,
+        Mark: 15,
+        Milana: 36,
+        Robert: 78,
+        Kate: 98,
+        Katrin: 95,
+    }
+    const  arr = {}
+    const studFun = (elem) => {
+      for (let key in elem){
+          if (elem[key] > 90){
+              arr[key] = elem[key]
+          }
+      }
+      return arr
+    }
+    const out = studFun(stud)
+    console.log(out)
 })()
