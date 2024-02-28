c0d27ca4c0b18a81e919e693ce3bcec0775f760c
litu3000 <acersytec@i.ua>
2024-02-28 18:40:09 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
+const parseJSON = function (a) {
+    try {
+        JSON.parse(a)
+        return true
+    } catch (error) {
+        return "Invalid JSON string"
+    }
+}
 
-console.log(parseJSON('{"name": "John", "age": 30}')); // повинно вивести об'єкт { name: 'John', age: 30 }
-console.log(parseJSON('{"name": "John", "age":')); // повинно вивести помилку "Invalid JSON string"
+console.log(parseJSON('{"name": "John", "age": }'))
