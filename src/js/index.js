be7bee8ca6e81b9d147a953b05c2d8fc8730508f
litu3000 <acersytec@i.ua>
2024-03-02 22:46:53 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
+const bodyEl = document.body
+const bodyChilds = bodyEl.childNodes
+const ul = bodyChilds[1]
+
+const arr = Array.from(ul.childNodes)
+let sum = null
+let result = arr.filter(
+    function (item) {
+        if (item.nodeName === 'LI') {
+            item.style.color = 'blueviolet'
+            sum += 1
+            console.log(item.innerHTML)
+            return item.innerHTML
+        }
+        return null
+    }
+)
+
+let pop = []
+for (let elem of result) {
+    pop.push(elem.innerHTML)
+}
+
+console.log(pop)
+console.log(sum)
 
