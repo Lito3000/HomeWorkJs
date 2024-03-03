12aba02b93f70b59025eba9cc149b51564b079da
litu3000 <acersytec@i.ua>
2024-03-03 20:26:29 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
+(function () {
+    const bodyEl = document.body
+    const input = document.getElementById('ulId')
+    let atributeArrayValue = []
+    let atributeArrayName = []
+    for (let atr of input.attributes) {
+        atributeArrayValue.push(atr.value)
+        atributeArrayName.push(atr.name)
+        input.firstElementChild.setAttribute('data-my-name', 'andrey')
+        input.removeAttribute('data-dog-tail')
+    }
+    console.log(atributeArrayValue)
+    console.log(atributeArrayName)
 
+    const bodyChilds = bodyEl.childNodes
+    const ul = bodyChilds[1]
+    const li = ul.childNodes[3]
+    li.innerHTML = 'Привет меня зовут Андрей'
+
+})()
