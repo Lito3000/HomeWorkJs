97ae59dbe896a184489c898591d722ae6193f456
litu3000 <acersytec@i.ua>
2024-03-04 15:20:55 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-
+"use strict";
+(function () {
+    const arr = [1, 2, [1.1, 1.2, 1.3], 3]
+    const generateList = function (item) {
+        const ul = document.createElement('ul')
+        for (let i = 0; i < item.length; i++) {
+            const li = document.createElement('li')
+            if (!Array.isArray(item[i])) {
+                li.innerHTML = item[i]
+            } else {
+                li.append(generateList(item[i]))
+            }
+            ul.append(li)
+        }
+        return ul
+    }
+    document.body.append(generateList(arr))
+})()
