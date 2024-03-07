fb1b92628dd0ea1e1fc76fc519c15f8593f1bb74
litu3000 <acersytec@i.ua>
2024-03-07 21:17:25 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
+"use strict";
+(function () {
+    const num = document.querySelector('[text]')
+    const btn = document.querySelector('[btn]')
 
+    btn.onclick = function () {
+        if (Number(num.value) % 2 === 0) {
+            alert('Парне число')
+        } else {
+            alert('Не парне число')
+        }
+    }
+})()
