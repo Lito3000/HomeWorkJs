12a02eb19fc65d9d5535c1a1658b681e150c2ed6
litu3000 <acersytec@i.ua>
2024-03-07 23:07:24 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
+"use strict";
+(function () {
+    const num = document.querySelector('[text]')
+    const btn = document.querySelector('.btn')
 
+    btn.onclick = function () {
+        const tegP = document.createElement('p')
+        tegP.innerHTML = num.value
+        document.body.append(tegP)
+        num.value = ''
+    }
+})()
