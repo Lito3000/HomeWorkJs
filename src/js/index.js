5a146717d895a95a04da82eeeff33dd3981b495a
litu3000 <acersytec@i.ua>
2024-03-15 22:11:05 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,37 @@
+'use strict';
+
+(function () {
+    const validation = (form) => {
+        const createErr = (inp, text) => {
+            const parent = inp.parentNode
+            const tegP = document.createElement('p')
+            tegP.innerHTML = text
+            tegP.classList.add('err-label')
+            parent.append(tegP)
+        }
+        let result = true
+        form.querySelectorAll('input').forEach(input => {
+            if (input.value === '') {
+                createErr(input, 'Будь ласка, заповніть усі поля')
+                result = false
+            }
+        })
+        return result
+    }
+    document.querySelector('[data-form]').addEventListener('submit', function (e) {
+        e.preventDefault()
+        const data = {}
+        if (validation(this) === true) {
+            const parent = this.parentNode
+            const tegP = document.createElement('p')
+            tegP.innerHTML = "Реєстрація успішна"
+            parent.append(tegP)
+            const inputs = e.target.querySelectorAll('input');
+            inputs.forEach(input => {
+                data[input.name] = input.value
+            })
+        }
+        console.log(data)
+    })
+})()
 
